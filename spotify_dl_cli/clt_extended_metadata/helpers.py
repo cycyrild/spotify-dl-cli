dae43b542532bfbd87e4664f738fb47bbbd2e339
Cyril DRIMARACCI <cyrildrimaracci@live.fr>
2026-03-28 11:55:57 +0100

    Refactor base62 encoding

--- spotify_dl_cli/clt_extended_metadata/helpers.py
+++ spotify_dl_cli/clt_extended_metadata/helpers.py
@@ -1,21 +1,35 @@
 BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-def _base62_encode(data: bytes) -> str:
-    num = int.from_bytes(data, byteorder="big")
+# https://github.com/librespot-org/librespot/blob/33bf3a77ed4b549df67e8347d7d6e55b007b3ec2/core/src/spotify_id.rs#L56
+def spotify_gid_to_base62(gid: bytes) -> str:
+    if len(gid) != 16:
+        raise ValueError("Spotify GID must be 16 bytes")
 
-    if num == 0:
-        return "0".rjust(22, "0")
+    n = int.from_bytes(gid, "big")
 
-    out = ""
-    while num:
-        num, rem = divmod(num, 62)
-        out = BASE62[rem] + out
+    dst = [0] * 22
+    i = 0
 
-    return out.rjust(22, "0")
+    for shift in (96, 64, 32, 0):
+        carry = (n >> shift) & 0xFFFFFFFF
+
+        j = 0
+        while j < i:
+            carry += dst[j] << 32
+            dst[j] = carry % 62
+            carry //= 62
+            j += 1
+
+        while carry > 0:
+            dst[i] = carry % 62
+            carry //= 62
+            i += 1
+
+    return "".join(BASE62[d] for d in reversed(dst))
 
 
 def track_gid_to_uri(track_gid: bytes) -> str:
     if len(track_gid) != 16:
         raise ValueError("track_gid must be exactly 16 bytes")
-    return f"spotify:track:{_base62_encode(track_gid)}"
+    return f"spotify:track:{spotify_gid_to_base62(track_gid)}"
